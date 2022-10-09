70ecbf6ae322f8ab8f0f69146f0b03bc1db53675
Bruno Domingues <bruno.domingues@lighthouseit.com.br>
2022-10-09 15:13:13 -0300

    feat: forms validated

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -22,13 +22,13 @@ interface TransactionsProviderProps {
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  useEffect(() => {
-    async function loadTransactions() {
-      const response = await fetch("http://localhost:3333/transactions");
-      const data = await response.json();
-      setTransactions(data);
-    }
+  async function loadTransactions() {
+    const response = await fetch("http://localhost:3333/transactions");
+    const data = await response.json();
+    setTransactions(data);
+  }
 
+  useEffect(() => {
     loadTransactions();
   }, []);
 
